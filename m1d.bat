eda16bdcb0e8fae7921585d89382cb6c737a0edf
Valmir Franca <vfranca3@gmail.com>
2019-01-17 05:32:53 -0200

    Adicionado filtro por data a funcao reader()

---
+++ m1d.bat
@@ -0,0 +1,3 @@
+@echo off
+py reader.py wdog19m1.csv 30
+time /t
--- reader.py
+++ reader.py
@@ -1,9 +1,23 @@
 from trade.reader import reader
-import sys
+import sys, re
 
-filename = "var/" + sys.argv[1]
-times = int(sys.argv[2])
+file = "var/"
 
-bars = reader(filename, times)
+if len(sys.argv) == 2:
+    file += sys.argv[1]
+    bars = reader(file)
+elif len(sys.argv) == 3:
+    file += sys.argv[1]
+    arg2 = sys.argv[2]
+    if re.match('^[0-9]*$', arg2):
+        bars = reader(file, times = int(arg2))
+    elif re.match(r'^[0-9]{4}.[0-9]{2}.[0-9]{2}$', arg2):
+        bars = reader(file, date = arg2)
+elif len(sys.argv) == 4:
+    file += sys.argv[1]
+    arg3 = int(sys.argv[3])
+    arg2 = sys.argv[2]
+    bars = reader(file, times = arg3, date = arg2)
+
 for bar in bars:
     print(bar)
