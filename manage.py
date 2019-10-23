8cf6e75c4be6a5f523d91d0898ca144b01fa395c
Valmir Franca <vfranca3@gmail.com>
2019-10-23 19:21:11 -0300

    Melhorado estilo de codigo manage.py

--- manage.py
+++ manage.py
@@ -5,13 +5,13 @@ from cli_trade.sma import ma
 from cli_trade.ema import get_ema
 from cli_trade._fib import Fib
 from cli_trade.atr import atr
-from cli_trade.conf import *
+from cli_trade import conf
 import sys
 import re
 
 
 if sys.argv[1] == "bars":
-    file = csv_path
+    file = conf.csv_path
     pattern_qtt = re.compile(r'^[0-9]*$')
     pattern_date = re.compile(r'^[0-9]{4}.[0-9]{2}.[0-9]{2}$')
     pattern_view = re.compile('^[a-z]*$')
@@ -24,11 +24,11 @@ if sys.argv[1] == "bars":
         file += sys.argv[2]
         arg2 = sys.argv[3]
         if pattern_qtt.match(arg2):
-            views = controller(file, qtt_bars = int(arg2))
+            views = controller(file, qtt_bars=int(arg2))
         elif pattern_date.match(arg2):
-            views = controller(file, date = arg2)
+            views = controller(file, date=arg2)
         elif pattern_view.match(arg2):
-            views = controller(file, view = arg2)
+            views = controller(file, view=arg2)
     # chamada com 3 argumentos
     elif len(sys.argv) == 5:
         file += sys.argv[2]
@@ -37,32 +37,32 @@ if sys.argv[1] == "bars":
         # Quando o 2o argumento é uma data
         if pattern_date.match(arg2):
             if pattern_qtt.match(arg3):
-                views = controller(file, date = arg2, qtt_bars = int(arg3))
+                views = controller(file, date=arg2, qtt_bars=int(arg3))
             if pattern_view.match(arg3):
-                views = controller(file, date = arg2, view = arg3)
+                views = controller(file, date=arg2, view=arg3)
         # Quando o 2o argumento é um modo de exibição
         elif pattern_view.match(arg2):
             if pattern_qtt.match(arg3):
-                views = controller(file, view = arg2, qtt_bars = int(arg3))
+                views = controller(file, view=arg2, qtt_bars=int(arg3))
             elif pattern_date.match(arg3):
-                views = controller(file, view = arg2, date = arg3)
+                views = controller(file, view=arg2, date=arg3)
     for view in views:
         print(view)
 
 if sys.argv[1] == "sma":
     times = int(sys.argv[2])
-    filename = csv_path + sys.argv[3]
+    filename = conf.csv_path + sys.argv[3]
     mms = ma(times, filename)
     print(mms)
 
 if sys.argv[1] == "ema":
     times = int(sys.argv[2])
-    filename = csv_path + sys.argv[3]
+    filename = conf.csv_path + sys.argv[3]
     mm = get_ema(times, filename)
     print(mm)
 
 if sys.argv[1] == "atr":
-    file = "%s%s.csv" % (csv_path, sys.argv[2])
+    file = "%s%s.csv" % (conf.csv_path, sys.argv[2])
     if len(sys.argv) == 4:
         candles = int(sys.argv[3])
     else:
