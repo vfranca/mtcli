3cc909a479cd117fa27b6398d13dcc4452bd28b2
Valmir Franca <vfranca3@gmail.com>
2019-10-27 11:01:32 -0300

    Atualizado README

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vfranca/cli-trade',
-    version='0.1a',
+    version='0.1a.post',
     zip_safe=False,
 )
