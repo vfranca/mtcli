dbdcc5c83abc5c28cc263fb41b81230fe807bfa2
Valmir Franca <vfranca3@gmail.com>
2019-11-20 07:00:20 -0300

    Implementado suporta a markdown no Sphinx

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vfranca/chartcli',
-    version='0.1.4a.post2',
+    version='0.2a0',
     zip_safe=False,
 )
