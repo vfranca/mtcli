73f3b0c7924923c7026fb15aae53e94acc8c1eda
Valmir Franca <vfranca3@gmail.com>
2020-05-12 03:12:56 -0300

    Revisada alias para visao de canal e de default

--- mtcli_arquivos/ch.bat
+++ mtcli_arquivos/ch.bat
@@ -2,10 +2,8 @@
 rem aliases para comandos do mtcli
 rem chamada da view channel
 if "%1" == "" (
-echo mt bars %t% --period %p% --date %d% --view ch --count 107
 mt bars %t% --period %p% --date %d% --view ch --count 107
 ) else (
-echo mt bars %t% --period %p% --date %d% --view %1 --count %2
 mt bars %t% --period %p% --date %d% --view %1 --count %2
 )
 mt sma %t% --period %p% --count 20
