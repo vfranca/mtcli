3d51ece6b28fdaa31999c91704ddc8a5f7acc200
Valmir Franca <vfranca3@gmail.com>
2020-05-13 01:00:05 -0300

    Adicionadas mensagens de confirmacao ao init.bat

--- mtcli_arquivos/init.bat
+++ mtcli_arquivos/init.bat
@@ -1,7 +1,14 @@
 @echo off
+rem aliases para comandos do mtcli
+rem inicia as variáveis de ambiente
 set t=petr4
+echo symbol PETR4
 set p=daily
+echo period Daily
 set d=""
+echo data vazio
 prompt $$
+echo alterado prompt
 title mtcli
-
+echo alterado titulo do terminal
+echo diretorio pronto para trabalho
