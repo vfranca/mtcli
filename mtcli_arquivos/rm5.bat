161363bc53eddb7b213742a7ead92b8650f9e89b
Valmir Franca <vfranca3@gmail.com>
2020-05-16 00:03:59 -0300

    Ajustada qutd de barras p o alias do grafico de ranges de m5 de 107 para 108 barras

--- mtcli_arquivos/rm5.bat
+++ mtcli_arquivos/rm5.bat
@@ -2,4 +2,4 @@
 rem aliases para comandos do mtcli
 rem grafico de ranges
 set p=m5
-mt bars %t% --view r --period %p% --date %d% --count 107
+mt bars %t% --view r --period %p% --date %d% --count 108
