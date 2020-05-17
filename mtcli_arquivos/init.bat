d1e605475a82a2de502bf3775d6954df3f232277
Valmir Franca <vfranca3@gmail.com>
2020-05-17 14:51:48 -0300

    Adicionada a opcao -l para listar as variaveis de ambiente

--- mtcli_arquivos/init.bat
+++ mtcli_arquivos/init.bat
@@ -1,9 +1,17 @@
 @echo off
 rem aliases para comandos do mtcli
+rem lista as variáveis de ambiente
+if "%1" == "-l" (
+echo symbol %t%
+echo period %p%
+echo date %d%
+goto :EOF
+)
+
 rem inicia as variáveis de ambiente
 set t=petr4
 echo symbol PETR4
-set p=daily
+set p=m15
 echo period Daily
 set d=""
 echo data vazio
