6846c1f9ec6b9b0f625d0c803f795cfa313feb70
Valmir Franca <vfranca3@gmail.com>
2020-06-04 02:41:08 -0300

    Atualizado o alias para o grafico default default.bat

--- mtcli_arquivos/default.bat
+++ mtcli_arquivos/default.bat
@@ -1,9 +1,11 @@
 @echo off
 rem aliases para comandos do mtcli
-rem view default
+rem gráfico default
+cls
 if "%1" == "" (
-mt bars %t% --period %p% --date %d% --count 107
+mt bars %t% --period %p% --date %d% --count 108
 ) else (
 mt bars %t% --period %p% --date %d% --view %1 --count %2
 )
 mt sma %t% --period %p% --count 20
+time /t
