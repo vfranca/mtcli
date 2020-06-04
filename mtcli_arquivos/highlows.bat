bef304bdbc3d3a8b44b5431ce8cd314164f07243
Valmir Franca <vfranca3@gmail.com>
2020-06-04 02:35:38 -0300

    Tornado deprecated o alias hl.bat e substituido por highlows.bat para o grafico de maximas e minimas

--- mtcli_arquivos/highlows.bat
+++ mtcli_arquivos/highlows.bat
@@ -1,9 +1,8 @@
 @echo off
-rem deprecated
 rem aliases para comandos do mtcli
 rem grafico de máximas e mínimas
-
-rem conteúdo do alias
+cls
+rem Exibe o comando do alias
 if "%1" == "-e" (
 echo mt bars %t% --period %p% --date %d% --view ch --count 108
 echo mt sma %t% --period %p% --count 20
@@ -14,11 +13,10 @@ echo mt bars %t% --period %p% --date %d% --view %1 --count %2
 echo mt sma %t% --period %p% --count 20
 goto :EOF
 )
-
 if "%1" == "" (
 mt bars %t% --period %p% --date %d% --view ch --count 108
 ) else (
 mt bars %t% --period %p% --date %d% --view %1 --count %2
 )
-
 mt sma %t% --period %p% --count 20
+time /t
