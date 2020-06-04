00d95e24aaa3183d2344f3e5141e1a2887a899bb
Valmir Franca <vfranca3@gmail.com>
2020-06-04 02:30:50 -0300

    Tornado deprecated r.bat e substituido por ranges.bat para alias do grafico de ranges

--- mtcli_arquivos/ranges.bat
+++ mtcli_arquivos/ranges.bat
@@ -1,13 +1,12 @@
 @echo off
-rem deprecated
 rem aliases para comandos do mtcli
-
-rem conteúdo do alias
+rem gráfico de ranges
+cls
+rem Exibe o comando do alias
 if "%1" == "-e" (
 echo mt bars %t% --period %p% --date %d% --view r --count 108
 echo mt atr %t% --period %p%
 goto :EOF
 )
-
 mt bars %t% --period %p% --date %d% --view r --count 108
 mt atr %t% --period %p%
