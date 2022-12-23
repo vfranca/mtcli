a913355c9e9f2f380619c46d2fc21e8dd40d2378
Valmir Franca <vfranca3@gmail.com>
2022-12-23 01:25:38 -0300

    Movida a rotina de calculo do range medio para mtcli.rm

--- mtcli/rm.py
+++ mtcli/rm.py
@@ -1,15 +1,28 @@
 import click
-from mtcli import indicator
+from mtcli.csv_data import Rates
+from mtcli.pa.pa_bar import Bar
+from mtcli import conf
 
 
 # Cria o comando rm
 @click.command()
 @click.argument("symbol")
-@click.option("--period", "-p", default="h1", help="Tempo gráfico")
+@click.option("--period", "-p", default="D1", help="Tempo gráfico")
 @click.option("--count", "-c", default=14, help="Quantidade de períodos")
 def rm(symbol, period, count):
-    """Calcula o range médio das barras."""
-    click.echo(indicator.atr.get_atr(symbol, period, count))
+    """Range médio das barras."""
+    csv_file = conf.csv_path + symbol + period + ".csv"
+    ranges = []
+    rates = Rates(csv_file)
+    for rate in rates:
+        bar = Bar(rate)
+        # Elimina doji de 4 preços
+        if bar.open == bar.high and bar.high == bar.low and bar.low == bar.close:
+            continue
+        ranges.append(bar.high - bar.low)
+    ranges = ranges[-count:]
+    rm = round(sum(ranges) / len(ranges), conf.digits)
+    click.echo(rm)
 
 
 if __name__ == "__main__":
