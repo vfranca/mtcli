e6c44902b2fe04d15a367423efc0257da84df012
Valmir França <vfranca3@gmail.com>
2025-03-04 19:18:55 -0300

    Revisados imports de set.py

--- mtcli/set.py
+++ mtcli/set.py
@@ -3,7 +3,7 @@ Gerencia configurações
 """
 
 import click
-from dotenv import dotenv_values, set_key
+import dotenv
 
 
 @click.command()
@@ -28,53 +28,53 @@ def set(**kwargs):
     env_file = "mtcli.ini"
     # Altera os dígitos da moeda
     if kwargs["digitos"]:
-        res = set_key(env_file, "DIGITOS", kwargs["digitos"])
+        res = dotenv.set_key(env_file, "DIGITOS", kwargs["digitos"])
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera o nome da barra lateral
     if kwargs["lateral"]:
-        res = set_key(env_file, "LATERAL", kwargs["lateral"].upper())
+        res = dotenv.set_key(env_file, "LATERAL", kwargs["lateral"].upper())
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera o nome da barra de alta
     if kwargs["alta"]:
-        res = set_key(env_file, "ALTA", kwargs["alta"].upper())
+        res = dotenv.set_key(env_file, "ALTA", kwargs["alta"].upper())
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera o nome da barra de baixa
     if kwargs["baixa"]:
-        res = set_key(env_file, "BAIXA", kwargs["baixa"].upper())
+        res = dotenv.set_key(env_file, "BAIXA", kwargs["baixa"].upper())
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera a abreviatura da barra de rompimento de alta
     if kwargs["rompimento_alta"]:
-        res = set_key(env_file, "ROMPIMENTO_ALTA", kwargs["rompimento_alta"].upper())
+        res = dotenv.set_key(env_file, "ROMPIMENTO_ALTA", kwargs["rompimento_alta"].upper())
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera a abreviatura da barra de rompimento de baixa
     if kwargs["rompimento_baixa"]:
-        res = set_key(env_file, "ROMPIMENTO_BAIXA", kwargs["rompimento_baixa"].upper())
+        res = dotenv.set_key(env_file, "ROMPIMENTO_BAIXA", kwargs["rompimento_baixa"].upper())
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera o percentual do corpo da barra doji
     if kwargs["percentual_doji"]:
-        res = set_key(env_file, "PERCENTUAL_DOJI", kwargs["percentual_doji"])
+        res = dotenv.set_key(env_file, "PERCENTUAL_DOJI", kwargs["percentual_doji"])
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera o percentual do corpo da barra de rompimento
     if kwargs["percentual_rompimento"]:
-        res = set_key(
+        res = dotenv.set_key(
             env_file, "PERCENTUAL_ROMPIMENTO", kwargs["percentual_rompimento"]
         )
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Altera o caminho da pasta do MT5
     if kwargs["mt5_pasta"]:
-        res = set_key(env_file, "MT5_PASTA", kwargs["mt5_pasta"])
+        res = dotenv.set_key(env_file, "MT5_PASTA", kwargs["mt5_pasta"])
         click.echo("%s=%s" % (res[1], res[2]))
         return 0
     # Lista as variáveis disponíveis
-    vars = dotenv_values(env_file)
+    vars = dotenv.dotenv_values(env_file)
     for var in vars.items():
         click.echo("%s=%s" % (var[0], var[1]))
 
