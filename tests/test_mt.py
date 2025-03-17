bdf924c345aae8b5e5bf1d9a54507b7a55d56990
Valmir França <vfranca3@gmail.com>
2025-03-17 16:35:28 -0300

    Adicionado teste para excecao de arquivo nao encontrado

--- tests/test_mt.py
+++ tests/test_mt.py
@@ -9,3 +9,8 @@ run = CliRunner()
 def test_exibe_a_versao():
     res = run.invoke(mt, ["--version"])
     assert res.output == "mtcli 0.28.2\n"
+
+
+def test_exibe_msg_de_excecao_quando_um_arquivo_csv_nao_e_encontrado():
+    res = run.invoke(mt, ["bars", "XXXX"])
+    assert res.output == "Grafico XXXXd1 nao encontrado! Tente novamente\n"
