1adcb7d44f9249eed268341dc2f5221ed5f10c85
Valmir França <vfranca3@gmail.com>
2025-03-21 04:24:52 -0300

    Adicionadas opcoes de timeframe ao comando mm

--- mtcli/mm.py
+++ mtcli/mm.py
@@ -10,7 +10,35 @@ from mtcli import conf
 
 @click.command()
 @click.argument("symbol")
-@click.option("--period", "-p", default="D1", help="Tempo grafico, default D1.")
+@click.option(
+    "--period",
+    "-p",
+    type=click.Choice(
+        [
+            "mn1",
+            "w1",
+            "d1",
+            "h4",
+            "h3",
+            "h2",
+            "h1",
+            "m30",
+            "m20",
+            "m15",
+            "m12",
+            "m10",
+            "m6",
+            "m5",
+            "m4",
+            "m3",
+            "m2",
+            "m1",
+        ],
+        case_sensitive=False,
+    ),
+    default="D1",
+    help="Tempo grafico, default D1.",
+)
 @click.option("--count", "-c", default=20, help="Quantidade de barras, default 20.")
 def mm(symbol, period, count):
     """Calcula a media movel simples."""
