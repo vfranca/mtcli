598ae087bcc1ca4dd4e76e4861d28c40d2520a4c
Valmir França <vfranca3@gmail.com>
2025-03-24 22:45:36 -0300

    Revisado script de build do executavel

--- build.bat
+++ build.bat
@@ -10,3 +10,11 @@ copy %dir_build%%mtcli% %dir_dist%%mtcli%
 copy %dir_build%%ma_txt% %dir_dist%%ma_txt%
 copy docs\%readme% %dir_dist%%readme%
 copy %license% %dir_dist%%license%
+
+
+set fzip=mtcli-%version%.zip
+cd %dir_dist%
+call zip -r %fzip% *.*
+move %fzip% ..\%fzip%
+cd ..\..\..
+rd /s /q %dir_dist%
