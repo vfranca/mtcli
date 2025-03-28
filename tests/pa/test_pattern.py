29273505da8d8d70f0f38f584904947bb858d340
Valmir França <vfranca3@gmail.com>
2025-03-28 03:08:00 -0300

    Revisados nomes de testes

--- tests/pa/test_pattern.py
+++ tests/pa/test_pattern.py
@@ -4,35 +4,35 @@ from mtcli import conf
 
 
 class TestPaOneBar(unittest.TestCase):
-    def test_body_pattern_bull(self):
+    def test_retorna_barra_de_alta(self):
         o = pattern.OneBar(50, 25, 25, 92600, 92500)
         self.assertEqual(o.body_pattern, conf.alta)
 
-    def test_body_pattern_bear(self):
+    def test_retorna_barra_de_baixa(self):
         o = pattern.OneBar(-50, 25, 25, 92500, 92600)
         self.assertEqual(o.body_pattern, conf.baixa)
 
-    def test_body_pattern_doji(self):
+    def test_retorna_barra_doji(self):
         o = pattern.OneBar(-5, 50, 45, 92500, 92600)
         self.assertEqual(o.body_pattern, conf.lateral)
 
-    def test_tail_top(self):
+    def test_retorna_sombra_superior(self):
         o = pattern.OneBar(-50, 20, 30, 92500, 92600)
         self.assertEqual(o.tail, conf.sombra_inferior)
 
-    def test_tail_bottom(self):
+    def test_retorna_sombra_inferior(self):
         o = pattern.OneBar(-50, 30, 20, 92500, 92600)
         self.assertEqual(o.tail, conf.sombra_superior)
 
-    def test_tail_neutral(self):
+    def test_retorna_sem_sombra(self):
         o = pattern.OneBar(-50, 25, 25, 92500, 92600)
         self.assertEqual(o.tail, "")
 
-    def test_buy_pressure(self):
+    def test_retorna_barra_de_rompimento_de_alta(self):
         o = pattern.OneBar(50, 20, 30, 92700, 92600)
         self.assertEqual(o.pattern, conf.rompimento_alta)
 
-    def test_sell_pressure(self):
+    def test_retorna_barra_de_rompimento_de_baixa(self):
         o = pattern.OneBar(-50, 25, 25, 92500, 92600)
         self.assertEqual(o.pattern, conf.rompimento_baixa)
 
@@ -41,16 +41,16 @@ class TestTwoBars(unittest.TestCase):
     def setUp(self):
         self.o = pattern.TwoBars([50, 60], [10, 20], [40, 50], [60, 80], [5, 15])
 
-    def test_close(self):
+    def test_retorna_os_fechamentos(self):
         self.assertEqual(self.o.close, [40, 50])
 
-    def test_high(self):
+    def test_retorna_as_maximas(self):
         self.assertEqual(self.o.high, [60, 80])
 
-    def test_low(self):
+    def test_retorna_as_minimas(self):
         self.assertEqual(self.o.low, [5, 15])
 
-    def test_pattern_bear_gap(self):
+    def test_retorna_gap_de_fechamento(self):
         o = pattern.TwoBars([-50, -60], [90, 40], [20, 10], [80, 60], [15, 5])
         self.assertEqual(o.pattern, "%s5.00" % "G")
 
