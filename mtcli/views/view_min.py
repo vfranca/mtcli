749e1fe6a58c78cbe38e97d6ad7472fbd5f774b1
Valmir França <vfranca3@gmail.com>
2025-04-12 20:37:48 -0300

    Refatorado view_min

--- mtcli/views/view_min.py
+++ mtcli/views/view_min.py
@@ -5,31 +5,36 @@ from mtcli import conf
 
 class MinView:
 
-    def __init__(self, bars, count, period = "d1", date = "", numerator = False, show_date = False):
+    def __init__(
+        self, bars, count, period="d1", date="", numerator=False, show_date=False
+    ):
         self.count = count
         self.period = period
         self.date = date
         self.numerator = numerator
         self.show_date = show_date
         self.chart = model_chart.ChartModel(bars, len(bars), count, date)
-        gaps, direcs = self.chart.get_padroes()
-        self.direcs = direcs[-count:]
         self.bars = bars[-count:]
 
     def views(self):
         views = []
         n = self.chart.get_n()
-        for bar, direc in zip(self.bars, self.direcs):
+        gaps, direcs = self.chart.get_padroes()
+        direcs = direcs[-self.count :]
+        for bar, direc in zip(self.bars, direcs):
             n += 1
             if self.numerator or (
-                self.show_date and (self.period == "d1" or self.period == "w1" or self.period == "mn1")
+                self.show_date
+                and (self.period == "d1" or self.period == "w1" or self.period == "mn1")
             ):  # numerador de barra ou data
                 view = "%s "  # numerador ou data
             else:
                 view = ""
             view += "%s %." + str(conf.digitos) + "f"  # máxima
             view += " %." + str(conf.digitos) + "f"  # mínima
-            if self.show_date and (self.period == "d1" or self.period == "w1" or self.period == "mn1"):
+            if self.show_date and (
+                self.period == "d1" or self.period == "w1" or self.period == "mn1"
+            ):
                 views.append(view % (bar.date, direc, bar.high, bar.low))
             elif self.numerator:
                 views.append(view % (n, direc, bar.high, bar.low))
