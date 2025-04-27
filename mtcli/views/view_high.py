5515e41add46a6489641dac13ee93f9a49e46bbb
Valmir França <vfranca3@gmail.com>
2025-04-27 05:40:58 -0300

    Adicionada hora a view highs no intraday

--- mtcli/views/view_high.py
+++ mtcli/views/view_high.py
@@ -25,18 +25,16 @@ class HighView:
         n = self.chart.get_n()
         for bar in self.bars:
             n += 1
-            if self.numerator or (
-                self.show_date
-                and (self.period == "d1" or self.period == "w1" or self.period == "mn1")
-            ):  # numerador de barra ou data
+            if self.numerator or self.show_date:
                 view = "%s "  # numerador ou data
             else:
                 view = ""
             view += "%." + str(conf.digitos) + "f"  # máxima
-            if self.show_date and (
-                self.period == "d1" or self.period == "w1" or self.period == "mn1"
-            ):
-                views.append(view % (bar.date, bar.high))
+            if self.show_date:
+                if self.period == "d1" or self.period == "w1" or self.period == "mn1":
+                    views.append(view % (bar.date, bar.high))
+                else:
+                    views.append(view % (bar.time, bar.high))
             elif self.numerator:
                 views.append(view % (n, bar.high))
             else:
