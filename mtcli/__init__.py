e66de84e67b43dca547be5fbc20948a66e869e0c
Valmir França <vfranca3@gmail.com>
2025-08-07 20:35:57 -0300

    Refatorado __init__.py

--- mtcli/__init__.py
+++ mtcli/__init__.py
@@ -1,3 +1,9 @@
 """Aplicativo de linha de comando para exibir graficos do MetaTrader 5 em texto."""
 
+from . import models
+from . import views
+from . import extensions
+from . import bars
+from . import conf
+from . import csv_data
 __version__ = "1.11.0"
