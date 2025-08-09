405eb54b72e59945c429957a1bb343679366424b
Valmir França <vfranca3@gmail.com>
2025-08-09 07:15:54 -0300

    Adicionados timeframes restantes a lista do mt5

--- mtcli/data/mt5.py
+++ mtcli/data/mt5.py
@@ -12,10 +12,26 @@ class MT5DataSource(DataSourceBase):
         """Retorna uma lista de lista de cotações do MetaTrader."""
         tf_map = {
             "M1": mt5.TIMEFRAME_M1,
+            "M2": mt5.TIMEFRAME_M2,
+            "M3": mt5.TIMEFRAME_M3,
+            "M4": mt5.TIMEFRAME_M4,
             "M5": mt5.TIMEFRAME_M5,
+            "M6": mt5.TIMEFRAME_M6,
+            "M10": mt5.TIMEFRAME_M10,
+            "M12": mt5.TIMEFRAME_M12,
             "M15": mt5.TIMEFRAME_M15,
+            "M20": mt5.TIMEFRAME_M20,
+            "M30": mt5.TIMEFRAME_M30,
             "H1": mt5.TIMEFRAME_H1,
+            "H2": mt5.TIMEFRAME_H2,
+            "H3": mt5.TIMEFRAME_H3,
+            "H4": mt5.TIMEFRAME_H4,
+            "H6": mt5.TIMEFRAME_H6,
+            "H8": mt5.TIMEFRAME_H8,
+            "H12": mt5.TIMEFRAME_H12,
             "D1": mt5.TIMEFRAME_D1,
+            "W1": mt5.TIMEFRAME_W1,
+            "MN1": mt5.TIMEFRAME_MN1,
         }
 
         if period not in tf_map:
