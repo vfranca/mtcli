0b67ec4958b1c2992cd32914d8050dedefe4ad5e
Valmir França <vfranca3@gmail.com>
2025-08-14 22:49:46 -0300

    Removendo subcomandos ma e sa

--- mtcli/__init__.py
+++ mtcli/__init__.py
@@ -1,6 +1,11 @@
 """Aplicativo de linha de comando para exibir graficos do MetaTrader 5 em texto."""
 
 from . import bars, conf, data, logger, models, views
-from .extensions import agressao, media_movel, moving_average, range_medio, volume_medio
+from .extensions import media_movel
+from .extensions import range_medio
+from .extensions import volume_medio
+# from .extensions import moving_average
+# from .extensions import agressao
+
 
 __version__ = "1.14.6"
